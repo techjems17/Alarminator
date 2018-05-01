ae79b7bce49cf81e758eb28bfa19f3bd56455e9a
Jordan Smith <techjems17@gmail.com>
2018-05-01 16:34:00 -0600

    added home page

--- app/App.js
+++ app/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Puzzles from "./Puzzles"
+import {
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+  Alert,
+  Text,
+  View,
+  Button,
+  Platform,
+  AlertIOS,
+  Picker,
+  TouchableOpacity,
+  Modal,
+  TouchableHighlight,
+  BackHandler,
+  Image,
+  Vibration,
+  VibrationIOS,
+} from 'react-native';
+import { StackNavigator} from 'react-navigation';
+console.disableYellowBox = true;
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Image style = {styles.image} source = {require('./assets/clocks.png')} />
       </View>
     );
   }
@@ -16,8 +35,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  image: {
+    width: 250,
+    height: 250,
+    marginBottom: 50,
   },
 });
