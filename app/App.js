7fd85e589f3c1d14c4127ae21ffed46b1030053b
Jordan Smith <techjems17@gmail.com>
2018-05-01 17:46:25 -0600

    added current time

--- app/App.js
+++ app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Puzzles from "./Puzzles"
+// import Puzzles from "./Puzzles"
 import {
   FlatList,
   StyleSheet,
@@ -19,14 +19,87 @@ import {
   Vibration,
   VibrationIOS,
 } from 'react-native';
-import { StackNavigator} from 'react-navigation';
+// import { StackNavigator} from 'react-navigation';
+// import DateTimePicker from 'react-native-modal-datetime-picker';
+import moment from 'moment';
+import { YellowBox } from 'react-native';
 console.disableYellowBox = true;
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isVisible: false,
+      time: ''
+    }
+  }
+
+  getTime() {
+    var date;
+    var timeType;
+    var hour;
+    var minutes;
+    var seconds;
+    var fullTime;
+
+    date = new Date();
+
+    hour = date.getHours();
+
+    if (hour == 0) {
+      hour = 12;
+    }
+
+    minutes = date.getMinutes();
+
+    if (minutes < 10) {
+      minutes = '0' + minutes.toString();
+    }
+
+    seconds = date.getSeconds();
+
+    if (seconds < 10) {
+      seconds = '0' + seconds.toString();
+    }
+
+    fullTime = hour.toString() + ':' + minutes.toString() + ':' + seconds.toString();
+
+    this.setState({
+      time: fullTime
+    });
+  }
+
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPressed', () => {
+      return true;
+    });
+    this.Clock = setInterval(() => this.getTime(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.Clock);
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Image style = {styles.image} source = {require('./assets/clocks.png')} />
+    return ( 
+      <View style = {styles.container}>
+        {/* <Text style={
+        {
+          color: 'yellow',
+          fontSize: 60,
+          marginBottom: 50,
+        }
+        } > {this.state.chosenTime} 
+        </Text> */}
+        <Image style = {styles.image} source = {require('./assets/clocks.png')}/> 
+        <Text style={
+        {
+          color: 'yellow',
+          fontSize: 60,
+          marginBottom: 50,
+        }
+        }> {this.state.time.toString()} 
+        </Text>
       </View>
     );
   }
@@ -46,3 +119,4 @@ const styles = StyleSheet.create({
     marginBottom: 50,
   },
 });
+
