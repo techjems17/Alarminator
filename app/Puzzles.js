ebefa9bc6a145b631d55e91a8f868a2ba647a1e6
Jordan Smith <techjems17@gmail.com>
2018-05-03 14:01:18 -0600

    added random puzzle to app

--- app/Puzzles.js
+++ app/Puzzles.js
@@ -26,6 +26,11 @@ export default class Puzzles extends Component {
         puzzles: [],
         userAnswer: "",
         incorrect: "",
+        shouldPlay: true,
+        isPlaying: true,
+        isLoading: false,
+        volume: 1.0,
+        rate: 1.5,
     };
 
     static navigationOptions = {
@@ -44,17 +49,96 @@ export default class Puzzles extends Component {
         BackHandler.addEventListener('hardwareBackPressed', () => {
             return true;
         });
+        fetch("http://localhost:3000/puzzles")
+            .then(response => {
+                return response.json()
+            })
+            .then(puzzles => {
+                var puzzle = puzzles[Math.floor(Math.random() * puzzles.length)]
+                this.setState({
+                    puzzles: puzzle.puzzle,
+                    answers: puzzle.answer,
+                })
+                console.log(puzzle.puzzle);
+            })
+            .catch(error => {
+                console.log(error)
+            });
+        // Audio.setAudioModeAsync({
+        //     allowsRecordingIOS: false,
+        //     interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        //     playsInSilentModeIOS: true,
+        //     shouldDuckAndroid: true,
+        //     interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+        // });
+        // this._loadNewPlaybackInstance(false);
     }
 
-    _onStop = () => {
-        return this.props.navigation.navigate('Home');
+    // async _loadNewPlaybackInstance(playing) {
+    //     if (this.playbackInstance != null) {
+    //         await this.playbackInstance.unloadAsync();
+    //         this.playbackInstance.setOnPlaybackStatusUpdate(null);
+    //         this.playbackInstance = null;
+    //     }
+
+    //     const source = { uri: 'https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Podington_Bear_-_Rubber_Robot.mp3' };
+    //     const initialStatus = {
+    //         shouldPlay: playing,
+    //         rate: this.state.rate,
+    //         volume: this.state.volume,
+    //     };
+
+    //     const { sound, status } = await Audio.Sound.create(
+    //         source,
+    //         initialStatus,
+    //         this._onPlaybackStatusUpdate
+    //     );
+    //     this.playbackInstance = sound;
+    // }
+
+    // _onPlaybackStatusUpdate = status => {
+    //     if (status.isLoaded) {
+    //         this.setState({
+    //             shouldPlay: status.shouldPlay,
+    //             isPlaying: status.isPlaying,
+    //             rate: status.rate,
+    //             volume: status.volume,
+    //         });
+    //         if (status.didJustFinish) {
+    //             this._updatePlaybackInstanceForIndex(true);
+    //         }
+    //     }
+    // };
+
+    updateAnswer = value => {
+        this.setState({
+            userAnswer: value,
+            incorrect: "",
+        })
     };
+
+    checkAnswer = () => {
+        if (this.state.userAnswer != undefined) {
+            if (this.state.userAnswer == this.state.answers) {
+                return this.props.navigation.navigate('Home');
+            }
+            else {
+                this.setState({
+                    incorrect: "Try Again"
+                })
+            }
+        }
+    };
+
 
     render() {
 
         return (
             <View style={styles.container}>
-                <TouchableOpacity style={styles.button} onPress={this._onStop}>
+                <Text style={styles.text}>{this.state.puzzles}</Text>
+                <Text style={styles.wrong}>{this.state.incorrect}</Text>
+                <TextInput style={styles.input} placeholder="Add answer" value={this.state.userAnswer} onChangeText={this.updateAnswer} />
+                <TouchableOpacity style={styles.button} onPress={this.checkAnswer}>
                     <Text style={styles.enter}>Check Solution</Text>
                 </TouchableOpacity>
             </View>
@@ -67,6 +151,23 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#ffff55',
         alignItems: 'center',
+    },
+    text: {
+        fontSize: 60,
+        fontWeight: 'bold',
+        // marginBottom: 50,
+        marginTop: 200,
+        justifyContent: 'center',
+    },
+    wrong: {
+        fontSize: 60,
+        fontWeight: 'bold',
+        marginBottom: 25,
+        marginTop: 50,
+    },
+    input: {
+        fontSize: 30,
+        color: 'black',
     },
     button: {
         width: 250,
